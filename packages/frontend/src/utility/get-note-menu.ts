6ef21eea456cd1cb2b0d91549255ddd3369aa1c8
na2na-p <na2na@na2na.dev>
2025-09-03 23:14:35 +0900

    リノートのLocalOnly無意味なので消す

--- packages/frontend/src/utility/get-note-menu.ts
+++ packages/frontend/src/utility/get-note-menu.ts
@@ -690,7 +690,6 @@ export function getRenoteMenu(props: {
 				}
 
 				const configuredVisibility = prefer.s.rememberNoteVisibility ? store.s.visibility : prefer.s.defaultNoteVisibility;
-				const localOnly = prefer.s.rememberNoteVisibility ? store.s.localOnly : prefer.s.defaultNoteLocalOnly;
 
 				let visibility = appearNote.visibility;
 				visibility = smallerVisibility(visibility, configuredVisibility);
@@ -700,7 +699,6 @@ export function getRenoteMenu(props: {
 
 				if (!props.mock) {
 					misskeyApi('notes/create', {
-						localOnly,
 						visibility,
 						renoteId: appearNote.id,
 					}).then((res) => {
